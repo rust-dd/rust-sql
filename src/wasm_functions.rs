b506f595dba69f3f62b433aeacfbc9d5533346d0
Daniel Boros <dancixx@gmail.com>
2023-12-12 09:21:26 +0100

    chore: remove unused C function

--- src/wasm_functions.rs
+++ src/wasm_functions.rs
@@ -4,7 +4,4 @@ use wasm_bindgen::prelude::*;
 extern "C" {
   #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
   pub(crate) async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-
-  #[wasm_bindgen(js_namespace = console)]
-  pub(crate) fn log(s: &str);
 }
